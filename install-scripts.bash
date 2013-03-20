5ada2661d2e6eb711a8faff4a0d5e80d013c1d2e
Flammie Pirinen <flammie@iki.fi>
2013-03-20 14:48:00 +0200

    Simple installation script

---
+++ install-scripts.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+# simple script to install all scripts to given path
+
+if test $# -gt 0 ; then
+    DEST=$1
+else
+    DEST=$HOME/bin/
+fi
+SCRIPTS="fetch-europarl.bash fetch-gutenberg.bash fetch-wikimedia.bash \
+    unpack-europarl.bash unpack-gutenbergs.bash unpack-wikimedia.bash"
+if ! test -d $DEST ; then
+    install -d  $DEST
+fi
+install -m 755 $SCRIPTS $DEST
+
